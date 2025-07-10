f9dcb7051f9f977c515db7e5618ce32fa55fa9f6
wyaming <wyaming89@gmail.com>
2025-07-10 11:58:40 +0000

    安装

---
+++ hello_world/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+    // 变量命名
+    let x = 5;
+    println!("The value of x is {}",x);
+    let mut y = 6;
+    println!("The value of y is {}", y);
+    y += 1;
+    println!("The new value of y is {}",y);
+}
--- 安装rust.md
+++ 安装rust.md
@@ -4,3 +4,11 @@
 ``` sh
 curl --proto 'https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 ```
+
+## 检查安装是否成功
+``` sh
+rustc -V
+cargo -V
+```
+
+# 认识Cargo
